2e7693e5386f93c3a83d45dc2e6e24c9a3378caf
Niccolò Albanese <nick_albanese@virgilio.it>
2025-01-13 16:25:13 +0100

    1 & 2 done

--- src/Ex2/Ex2_main.java
+++ src/Ex2/Ex2_main.java
@@ -1,4 +1,7 @@
 package Ex2;
 
 public class Ex2_main {
+	public static void main (String[] args){
+		Ex_2.InsertData();
+	}
 }
--- src/Ex2/Ex_2.java
+++ src/Ex2/Ex_2.java
@@ -1,4 +1,28 @@
 package Ex2;
 
+import java.util.Scanner;
+
 public class Ex_2 {
+
+	public static void InsertData(){
+		Scanner myscanner = new Scanner(System.in);
+
+		System.out.println("inserisci i km percorsi");
+		int km = myscanner.nextInt();
+		System.out.println("inserisci i litri di carburante consumati");
+		int litri = myscanner.nextInt();
+		CalcoloKmLitro(km,litri);
+	}
+	public static void CalcoloKmLitro(int km, int litri){
+		try{
+			if(litri==0){
+				throw new Exception("non è possibile eseguire l'operazione");
+			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		int CalcoloKmLitro= km/litri;
+		System.out.println("il consumo è pari a "+CalcoloKmLitro+"km/lt");
+	}
 }
